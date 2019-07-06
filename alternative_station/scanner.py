5eeeb3c282f62b0fb55d774c3491cd6c96441945
alternative_backend <alternative_backend@wp.pl>
2019-07-06 19:24:06 +0200

    making wide range for serial port

--- alternative_station/scanner.py
+++ alternative_station/scanner.py
@@ -17,8 +17,9 @@ class BarcodeScanner:
             self.MasterModule.flush()
 
     def init_connection(self):
-        _ports = ['/dev/ttyUSB{}'.format(number) for number in range(0, 20)]
-        
+        _ports = ['/dev/ttyUSB{}'.format(number) for number in range(0, 20)] +
+                 ['/dev/ttyAMA{}'.format(number) for number in range(0, 30)]
+
         for port in _ports:
             try:
                 self.MasterModule = serial.Serial(port, 
